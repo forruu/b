1c60a95949201ebf421ca46b497062e5ebf6937d
forruu <fandystupa@gmail.com>
2024-09-20 22:26:32 +0700

    last

---
+++ b.js
@@ -0,0 +1,15 @@
+function openGift() {
+          
+    document.getElementById('modalGift').style.display = 'block';
+}
+
+function closeModalGift() {
+
+    document.getElementById('modalGift').style.display = 'none';
+    document.getElementById('modalMaaf').style.display = 'block';
+}
+
+function closeModalMaaf() {
+    document.getElementById('modalMaaf').style.display = 'none';
+
+}
